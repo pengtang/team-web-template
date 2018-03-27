502752919ce500aeea12cb4890e6c85335785191
Peng Tang <ptang@dstsystems.com>
2018-03-27 10:43:55 -0500

    restructure project

--- project/home/views.py
+++ project/home/views.py
@@ -1,6 +1,4 @@
-import os
-from flask import render_template, Blueprint, send_from_directory
-# from flask.ext.login import login_required, current_user
+from flask import Blueprint, render_template
 
 
 home_blueprint = Blueprint(
@@ -9,16 +7,8 @@ home_blueprint = Blueprint(
 )
 
 
-# @home_blueprint.route('/upload/<filename>')
-# def send_image(filename):
-#     return send_from_directory('images', filename)
-
-
-# use decorators to link the function to a url
 @home_blueprint.route('/', methods=['GET', 'POST'])
-# @login_required
 def home():
-    # base_path = os.path.dirname(os.path.abspath(__file__))
-    # images = os.listdir('project/home/images')
-    # print(images)
     return render_template('index.html')
+
+
