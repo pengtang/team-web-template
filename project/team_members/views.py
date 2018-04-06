cb597a247ff8e3d08ef0ec0ec76e57cb2a48029d
Peng Tang <ptang@dstsystems.com>
2018-04-06 16:14:56 -0500

    modify team members page to be compatible with front end

--- project/team_members/views.py
+++ project/team_members/views.py
@@ -14,6 +14,36 @@ def team_members():
     return render_template('team_members.html')
 
 
+@team_members_blueprint.route('/team_members/managers')
+def managers():
+    return render_template('managers.html')
+
+
+@team_members_blueprint.route('/team_members/data_scientists')
+def data_scientists():
+    return render_template('data_scientists.html')
+
+
+@team_members_blueprint.route('/team_members/data_engineers')
+def data_engineers():
+    return render_template('data_engineers.html')
+
+
+@team_members_blueprint.route('/team_members/qa_engineers')
+def qa_engineers():
+    return render_template('qa_engineers.html')
+
+
+@team_members_blueprint.route('/team_members/devops_engineers')
+def devops_engineers():
+    return render_template('devops_engineers.html')
+
+
+@team_members_blueprint.route('/team_members/project_management')
+def project_management():
+    return render_template('project_management.html')
+
+
 @team_members_blueprint.route('/team_members/Maryssa_Metheny', methods=['GET', 'POST'])
 # @login_required
 def maryssa():
